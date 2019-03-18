99546e4e2503364933a9247e29e15b07751dca93
Morten Larsen <baunegaard@live.dk>
2019-03-18 13:53:00 +0100

    Update nvm install script

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension shinnn.stylelint
 
 # Install Node Version Manager, Node.js and Yarn
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
